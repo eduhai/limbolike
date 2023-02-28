cc511806e39d65ebbfa7eaba2e08713654016a8b
vastapuum <mvastapuu@gmail.com>
2023-02-28 15:27:30 +0200

    scriptejä

--- Assets/Scripts/Player/CollisionScript.cs
+++ Assets/Scripts/Player/CollisionScript.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class CollisionScript : MonoBehaviour
 {
+    public PlayerPointsInfo playerPoints;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Portal")
             SceneManager.LoadScene("1-1");
+        else if (collision.collider.tag == "Crystal")
+        {
+            Debug.Log("kristalli");
+            playerPoints.value++;
+            collision.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/Player/PlayerPointsInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player Points Info")]
+public class PlayerPointsInfo : ScriptableObject
+{
+    public int value;
+}
